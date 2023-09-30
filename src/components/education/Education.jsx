b2b7a489b0d1fd08520088da3f810950b5242827
mattdinata <matt.dinata@gmail.com>
2023-09-30 11:23:24 +0800

    feat: add linting and update experiences

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Education.css'
+import React from 'react';
+import './Education.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -8,48 +8,122 @@ AOS.init();
 
 const Education = () => {
   return (
-    <section id="education" data-aos="zoom-in-up">
+    <section
+      id='education'
+      data-aos='zoom-in-up'
+    >
       <h2>EDUCATION</h2>
 
-      <div className="container education__container" style={{paddingBottom: "3rem"}}>
-        <div className="education__subheading">
+      <div
+        className='container education__container'
+        style={{ paddingBottom: '3rem' }}
+      >
+        <div className='education__subheading'>
           <h4>Nanyang Technological University</h4>
-          <h5 style={{fontWeight: "500", fontStyle: "italic", color: "var(--color-highlight)"}}>Aug 2021 - Present</h5>
+          <h5
+            style={{
+              fontWeight: '500',
+              fontStyle: 'italic',
+              color: 'var(--color-highlight)',
+            }}
+          >
+            Aug 2021 - Present
+          </h5>
         </div>
-        <div className="education__subheading">
+        <div className='education__subheading'>
           <h5>Bachelor of Electrical and Electronic Engineering</h5>
         </div>
 
-        <div className="education__content">
+        <div className='education__content'>
           <ul>
             <li>ASEAN Undergraduate Scholarship awardee</li>
-            <li>Dean's List AY2021/2022</li>
-            <li>Relevant Modules:<br/>
-              <p style={{fontSize: "0.7rem", lineHeight: "1.7", paddingTop: "3px"}}>
-                <i>From Computational Thinking to Programming &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; Data Structures & Algorithms &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; Introduction to Data Science and Artificial Intelligence</i>
+            <li>2x Dean's List (AY21/22 & AY22/23)</li>
+
+            <li>
+              Relevant Modules:
+              <br />
+              <p
+                style={{
+                  fontSize: '0.7rem',
+                  lineHeight: '1.7',
+                  paddingTop: '3px',
+                }}
+              >
+                <i>
+                  From Computational Thinking to Programming
+                  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; Data Structures &
+                  Algorithms &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; Introduction
+                  to Data Science and Artificial Intelligence
+                </i>
               </p>
             </li>
           </ul>
         </div>
       </div>
 
-      <div className="container education__container" style={{paddingTop: "10rem;"}}>
-        <div className="education__subheading">
+      <div
+        className='container education__container'
+        style={{ paddingTop: '10rem;' }}
+      >
+        <div className='education__subheading'>
           <h4>Online Courses & Certifications</h4>
         </div>
 
-        <div className="education__content">
+        <div className='education__content'>
           <ul>
-            <li>Build Web Apps with React & Firebase<em style={{paddingLeft: "1.5rem", fontStyle: "normal", color: "var(--color-highlight)"}}>by Shaun Pelling, Udemy</em></li>
-            <li>JavaScript Algorithms and Data Structures<em style={{paddingLeft: "1.5rem", fontStyle: "normal", color: "var(--color-highlight)"}}>by freeCodeCamp</em></li>
-            <li>Responsive Web Design<em style={{paddingLeft: "1.5rem", fontStyle: "normal", color: "var(--color-highlight)"}}>by freeCodeCamp</em></li>
-            <li>Complete Python Bootcamp From Zero to Hero in Python<em style={{paddingLeft: "1.5rem", fontStyle: "normal", color: "var(--color-highlight)"}}>by Jose Portilla, Udemy</em></li>
+            <li>
+              Build Web Apps with React & Firebase
+              <em
+                style={{
+                  paddingLeft: '1.5rem',
+                  fontStyle: 'normal',
+                  color: 'var(--color-highlight)',
+                }}
+              >
+                by Shaun Pelling, Udemy
+              </em>
+            </li>
+            <li>
+              JavaScript Algorithms and Data Structures
+              <em
+                style={{
+                  paddingLeft: '1.5rem',
+                  fontStyle: 'normal',
+                  color: 'var(--color-highlight)',
+                }}
+              >
+                by freeCodeCamp
+              </em>
+            </li>
+            <li>
+              Responsive Web Design
+              <em
+                style={{
+                  paddingLeft: '1.5rem',
+                  fontStyle: 'normal',
+                  color: 'var(--color-highlight)',
+                }}
+              >
+                by freeCodeCamp
+              </em>
+            </li>
+            <li>
+              Complete Python Bootcamp From Zero to Hero in Python
+              <em
+                style={{
+                  paddingLeft: '1.5rem',
+                  fontStyle: 'normal',
+                  color: 'var(--color-highlight)',
+                }}
+              >
+                by Jose Portilla, Udemy
+              </em>
+            </li>
           </ul>
         </div>
       </div>
-      
     </section>
-  )
-}
+  );
+};
 
-export default Education
+export default Education;
--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Experiences.css'
+import React from 'react';
+import './Experiences.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -8,43 +8,44 @@ AOS.init();
 
 const Experiences = () => {
   return (
-    <section id="experiences">
-      <div data-aos="zoom-in-up">
-      <h2 style={{textAlign: 'center'}}>ACTIVITIES</h2>
+    <section id='experiences'>
+      <div data-aos='zoom-in-up'>
+        <h2 style={{ textAlign: 'center' }}>WORK EXPERIENCES</h2>
 
+        <div className='container experiences__container'>
+          <div className='experiences__subheading'>
+            <h4>GIC</h4>
+            <h5
+              className='date'
+              style={{ fontWeight: '500', fontStyle: 'italic' }}
+            >
+              July 2023 - Present
+            </h5>
+          </div>
+          <div className='experiences__subtitle'>
+            <h5>Software Engineer Intern</h5>
+            <h6>Fixed Income & Multi-Asset Department</h6>
+          </div>
+        </div>
 
-      <div className="container experiences__container">
-        <div className="experiences__subheading">
-          <h4>PINTU IT</h4>
-          <h5 className="date" style={{fontWeight: "500", fontStyle: "italic"}}>Oct 2022 - Present</h5>
+        <div className='container experiences__container'>
+          <div className='experiences__subheading'>
+            <h4>Singtel</h4>
+            <h5
+              className='date'
+              style={{ fontWeight: '500', fontStyle: 'italic' }}
+            >
+              May 2023 - July 2023
+            </h5>
+          </div>
+          <div className='experiences__subtitle'>
+            <h5>Software Engineering Intern</h5>
+            <h6>Mobile Engineering Department</h6>
+          </div>
         </div>
-        <div className="experiences__subheading">
-          <h5>Technology Committee</h5>
-        </div>
-      </div>
-
-      <div className="container experiences__container">
-        <div className="experiences__subheading">
-          <h4>EEE Club</h4>
-          <h5 className="date" style={{fontWeight: "500", fontStyle: "italic"}}>Sep 2022 - Present</h5>
-        </div>
-        <div className="experiences__subheading">
-          <h5>Student Life Director</h5>
-        </div>
-      </div>
-
-      <div className="container experiences__container">
-        <div className="experiences__subheading">
-          <h4>PINTU Creative Media</h4>
-          <h5 className="date" style={{fontWeight: "500", fontStyle: "italic"}}>Oct 2021 - Oct 2022</h5>
-        </div>
-        <div className="experiences__subheading">
-          <h5>UI/UX Committee</h5>
-        </div>
-      </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experiences
+export default Experiences;
