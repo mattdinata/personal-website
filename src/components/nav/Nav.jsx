e1c56da559b89045b1d2b0e4c16f253ef317b381
mattdinata <matt.dinata@gmail.com>
2023-09-30 11:24:29 +0800

    feat: update nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,19 +1,25 @@
-import './Nav.css'
+import './Nav.css';
 
 const Nav = () => {
-  
   return (
-    <div className="main-nav animate__animated animate__fadeIn animate__slower animate__delay-1s">
-    
-      <ul className="nav">
-        <li><a href="/#">01. home</a></li>
-        <li><a href="#education">02. education</a></li>
-        <li><a href="#experiences">03. activities</a></li>
-        <li><a href="#projects">04. projects</a></li>
+    <div className='main-nav animate__animated animate__fadeIn animate__slower animate__delay-1s'>
+      <ul className='nav'>
+        <li>
+          <a href='/#'>01. home</a>
+        </li>
+        <li>
+          <a href='#education'>02. education</a>
+        </li>
+        <li>
+          <a href='#experiences'>03. experiences</a>
+        </li>
+        <li>
+          <a href='#projects'>04. projects</a>
+        </li>
         <li>______</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
